0c0f7545cc6e495fa9c137f428128c2666dccb00
xiaoyifan <xiaoyifan@uchicago.edu>
2015-11-25 17:33:36 -0600

    Recommit from api call.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,14 +1,7 @@
-require 'open-uri'
-
 class ProductsController < ApplicationController
 
-   def index
-  #@products = Product.all
-     json = open("http://localhost:4000/inventory.json").read
-    puts "###################"
-    puts json
-    puts "###################"
-    @products = JSON.parse(json)
-   end
+  def index
+     @products = Product.limit(1000)
+  end
 
 end
